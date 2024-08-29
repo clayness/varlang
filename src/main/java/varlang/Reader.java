8cbbf2f8632c4bf3605db6d570d82d85390427e4
Clay Stevens <clay.stevens@gmail.com>
2024-08-29 14:25:06 -0500

    updating to remove define lang (other repo) (#1)

--- src/main/java/varlang/Reader.java
+++ src/main/java/varlang/Reader.java
@@ -1,25 +1,68 @@
 package varlang;
 
+import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.Lexer;
 import varlang.AST.Program;
 import varlang.parser.VarLangLexer;
 import varlang.parser.VarLangParser;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Reader {
-    Program read() throws IOException {
+
+    @SuppressWarnings("SameReturnValue")
+    protected String getProgramDirectory() {
+        return "src/main/java/varlang/examples/";
+    }
+
+    public Program read() throws IOException {
         String programText = readNextProgram();
-        VarLangLexer l = new VarLangLexer(CharStreams.fromString(programText));
-        VarLangParser p = new VarLangParser(new org.antlr.v4.runtime.CommonTokenStream(l));
+        return parse(programText);
+    }
+
+    public Program parse(String programText) {
+        Lexer l = getLexer(CharStreams.fromString(programText));
+        VarLangParser p = getParser(new org.antlr.v4.runtime.CommonTokenStream(l));
         return p.program().ast;
     }
 
-    private String readNextProgram() throws IOException {
+    protected Lexer getLexer(CharStream s) {
+        return new VarLangLexer(s);
+    }
+
+    protected VarLangParser getParser(org.antlr.v4.runtime.CommonTokenStream s) {
+        return new VarLangParser(s);
+    }
+
+    protected String readNextProgram() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("$ ");
-        return br.readLine();
+        String programText = br.readLine();
+        return runFile(programText);
+    }
+
+    protected String runFile(String programText) throws IOException {
+        if (programText.startsWith("run ")) {
+            programText = readFile(getProgramDirectory() + programText.substring(4));
+        }
+        return programText;
+    }
+
+    private String readFile(String fileName) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+            return sb.toString();
+        }
     }
 }
